a30a4c0c4292a4118214149846cd9bf97703665c
Pat Cavit <pcavit@gmail.com>
2013-04-20 01:15:13 -0700

    Skeleton
    Spooky!

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+/*jshint node:true */
+"use strict";
+
+module.exports = function(grunt) {
+    
+    var nwDir = grunt.option("nwdir") || "./bin/node-webkit-v0.5.0-win-ia32/";
+    
+    grunt.loadNpmTasks("grunt-contrib-compress");
+    grunt.loadNpmTasks("grunt-shell");
+    
+    grunt.initConfig({
+        compress : {
+            chirrup : {
+                src  : [ "src/*", "package.json" ],
+                options : {
+                    mode    : "zip",
+                    archive : "chirrup.nw"
+                }
+            }
+        },
+        
+        shell : {
+            launch : {
+                command : "nw.exe ../../chirrup.nw",
+                options : {
+                    execOptions : {
+                        cwd : nwDir
+                    }
+                }
+            },
+            
+            package : {
+                command : "copy /by " + nwDir + "nw.exe+chirrup.nw chirrup.exe"
+            }
+        }
+    });
+    
+    grunt.registerTask("default", [ "compress", "shell:launch" ]);
+    grunt.registerTask("package", [ "compress", "shell:package" ]);
+};
