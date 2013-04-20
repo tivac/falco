ee4f1fce3cf1ecd42499d1f9048a68a2b5385f74
Pat Cavit <pcavit@gmail.com>
2013-04-20 14:54:33 -0700

    Cross-platform (ish) packaging
    Buffers are fun!
    Also now building into ./build/

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,31 @@
 /*jshint node:true */
 "use strict";
 
+var fs    = require("fs"),
+    async = require("async");
+
 module.exports = function(grunt) {
     
     var nwDir = grunt.option("nwdir") || "./bin/node-webkit-v0.5.0-win-ia32/";
     
     grunt.loadNpmTasks("grunt-contrib-compress");
-    grunt.loadNpmTasks("grunt-shell");
+    grunt.loadNpmTasks("grunt-mkdir");
     
     grunt.initConfig({
+        mkdir : {
+            build : {
+                options : {
+                    create : [ "build" ]
+                }
+            }
+        },
+        
         compress : {
-            chirrup : {
+            tristis : {
                 src  : [ "src/*", "package.json" ],
                 options : {
                     mode    : "zip",
-                    archive : "chirrup.nw"
+                    archive : "./build/tristis.nw"
                 }
             }
         },
@@ -36,15 +47,58 @@ module.exports = function(grunt) {
                         cwd : nwDir
                     }
                 }
-            },
-            
-            package : {
-                command : "copy /by " + nwDir + "nw.exe+chirrup.nw chirrup.exe"
             }
         }
+    });
+
+    // NodeJS powered binary concatenation, a bit longer than "copy /BY" but cross-platform :D
+    grunt.registerTask("package", "Build a packaged binary", function() {
+        var done = this.async();
+        
+        grunt.task.requires("mkdir");
+        grunt.task.requires("compress");
+        
+        grunt.log.writeln("Reading buffers");
+        
+        async.waterfall([
+            function readNodeWebkit(callback) {
+                fs.readFile(nwDir + "nw.exe", function(err, data) {
+                    if(err) {
+                        return callback(err);
+                    }
+                    
+                    callback(null, data);
+                });
+            },
+            
+            function readTristis(nw, callback) {
+                fs.readFile("./build/tristis.nw", function(err, data) {
+                    if(err) {
+                        return callback(err);
+                    }
+                    
+                    callback(null, nw, data);
+                });
+            },
+            
+            function combine(nw, tristis, callback) {
+                callback(null, Buffer.concat([ nw, tristis ]));
+            },
+            
+            function write(tristis, callback) {
+                fs.writeFile("./build/tristis.exe", tristis, function(err) {
+                    callback(err);
+                });
+            }
+        ], function(err) {
+            console.error(err);
+            
+            done();
+        });
+        
     });
     
     grunt.registerTask("default", [ "shell:launch" ]);
     grunt.registerTask("debug",   [ "shell:debug" ]);
-    grunt.registerTask("package", [ "compress", "shell:package" ]);
+    grunt.registerTask("release", [ "mkdir", "compress", "package" ]);
 };
