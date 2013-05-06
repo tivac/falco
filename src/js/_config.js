929a4e65334ddb0071299e8c790b3351ebf7683d
Pat Cavit <pcavit@gmail.com>
2013-05-06 00:40:34 -0700

    YUI config update
    Generated, then fixed by hand.
    
    o_0

--- src/js/_config.js
+++ src/js/_config.js
@@ -1,7 +1,18 @@
-/*exported tristis_config */
 var tristis_config = {
         base: "../node_modules/yui/",
+        filter: "raw",
         groups: {
+            "template": {
+                patterns: {
+                    "template-": {
+                        configFn: function (me) {
+                            "use strict";
+                            me.fullpath = "templates/compiled/" + me.name.replace(me.group + "-", "") + ".micro";
+                            me.requires = ["template"];
+                        }
+                    }
+                }
+            },
             "/": {
                 base: "js/",
                 modules: {
@@ -11,7 +22,11 @@ var tristis_config = {
                             "base-build",
                             "app",
                             "extension-tristis-events",
-                            "extension-tristis-routes"
+                            "extension-tristis-routes",
+                            "extension-view-classes",
+                            "extension-view-parent",
+                            "view-nav",
+                            "model-user"
                         ]
                     },
                     "extension-tristis-events": {
@@ -21,29 +36,42 @@ var tristis_config = {
                     "extension-tristis-routes": {
                         path: "extension-tristis-routes.js",
                         requires: []
+                    },
+                    "extension-view-classes": {
+                        path: "extension-view-classes.js",
+                        requires: ["event-custom"]
+                    },
+                    "extension-view-parent": {
+                        path: "extension-view-parent.js",
+                        requires: [
+                            "view",
+                            "event-custom"
+                        ]
+                    },
+                    "model-user": {
+                        path : "model-user.js",
+                        requires : [
+                            // YUI
+                            "base-build",
+                            "model"
+                        ]
+                    },
+                    "view-nav": {
+                        path: "view-nav.js",
+                        requires: [
+                            "base-build",
+                            "view",
+                            "template-nav"
+                        ]
                     }
                 }
             },
-            
             "/oauth/": {
                 base: "js/oauth/",
                 modules: {
                     "auth": {
                         path: "auth.js",
                         requires: ["node"]
-                    }
-                }
-            },
-            
-            "template": {
-                patterns : {
-                    "template-" : {
-                        configFn : function(me) {
-                            "use strict";
-                            
-                            me.fullpath = "templates/compiled/" + me.name.replace(me.group + "-", "") + ".micro";
-                            me.requires = [ "template" ];
-                        }
                     }
                 }
             }
