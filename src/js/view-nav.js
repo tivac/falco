e86cc1bd9d0e22d7821ac2b3f63694b646ac3a3e
Pat Cavit <pcavit@gmail.com>
2013-05-10 00:15:02 -0700

    Rendering lists in nav
    + handling events (broken though atm)

--- src/js/view-nav.js
+++ src/js/view-nav.js
@@ -1,36 +1,67 @@
 YUI.add("view-nav", function(Y) {
     "use strict";
     
-    var tristis   = Y.namespace("Tristis"),
+    var models    = Y.namespace("Tristis.Models"),
         templates = Y.namespace("Tristis.Templates"),
         Nav;
     
     Nav = Y.Base.create("nav", Y.View, [], {
         
+        events : {
+            "a" : {
+                click : "_navClick"
+            }
+        },
+        
         template : templates.nav,
         
         initializer : function() {
             this._handles = [
-                tristis.user.on("change", this.render, this)
+                models.user.after("change", this.render, this),
+                models.lists.after([ "change", "reset" ], this._renderLists, this)
             ];
         },
         
         destructor : function() {
             new Y.EventTarget(this._handles).detach();
             
-            this._handles = null;
+            this._handles = this._linkEvent = null;
         },
         
         render : function() {
             this.get("container").setHTML(
                 this.template({
-                    user : tristis.user.toJSON()
+                    user : models.user.toJSON()
+                })
+            );
+            
+            this._renderLists();
+        },
+        
+        _renderLists : function() {
+            this.get("container").one(".lists").setHTML(
+                templates["nav-lists"]({
+                    lists : models.lists.toJSON()
                 })
             );
         },
         
-        _renderLists : function(e) {
+        // DOM Events
+        _navClick : function(e) {
+            e.preventDefault();
             
+            if(!this._linkEvent) {
+                this._linkEvent = this.publish("link", {
+                    preventable : false
+                });
+            }
+            
+            
+            this.fire("link", {
+                // Using "getAttribute" here instead of "get" so we get the
+                // actual value instead of an absolute URL
+                url : e.target.getAttribute("href")
+            });
         }
     });
     
