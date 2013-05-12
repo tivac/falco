22db11df2d0d28fe857ab2bec5e885fd9d272cf2
Pat Cavit <pcavit@gmail.com>
2013-05-12 15:26:20 -0700

    Base timeline model (home/lists)

--- src/js/model-timeline-base.js
+++ src/js/model-timeline-base.js
@@ -1,61 +1,57 @@
-YUI.add("model-list-timeline", function(Y) {
+YUI.add("model-timeline-base", function(Y) {
     "use strict";
     
-    var tristis = Y.namespace("Tristis"),
-        models  = Y.namespace("Tristis.Models"),
-        Timeline;
+    var models = Y.namespace("Tristis.Models"),
+        TimelineBase;
         
-    Timeline = Y.Base.create("timeline", Y.LazyModelList, [], {
+    TimelineBase = Y.Base.create("timeline", Y.Model, [], {
+        initializer : function() {
+            var tweets;
+            
+            tweets = new models.Tweets();
+            
+            this.set("tweets", tweets);
+            
+            this._handles = [
+                tweets.after([ "reset", "more", "add" ], this._tweetAdd, this)
+            ];
+            
+            this.publish("tweets", { preventable : false });
+        },
         
         destructor : function() {
-            this.stop();
+            new Y.EventTarget(this._handles).detach();
             
-            this._stream = null;
+            this._handles = null;
         },
         
-        sync : function(action, options, done) {
-            tristis.twitter.get("statuses/home_timeline", {
-                count : 50,
-                contributor_details : true
-            }, done);
+        // Override .toJSON() to make sure tweets are included
+        toJSON : function() {
+            var json = List.superclass.toJSON.apply(this);
+            
+            json.tweets = json.tweets.toJSON();
+            
+            return json;
         },
         
-        stream : function() {
-            var self = this,
-                stream;
+        _tweetAdd : function(e) {
+            console.log(this.name, "_tweetAdd", e.type, e);
             
-            stream = tristis.twitter.stream("user", {
-                with : "followings"
+            this.fire("tweets", {
+                count : e.response.length || 1
             });
-            
-            // Only caring about tweets atm
-            stream.on("tweet", function(data) {
-                self.add(data);
-            });
-            
-            this._stream = stream;
-        },
-        
-        stop : function() {
-            if(this._stream) {
-                this._stream.stop();
-            }
-        },
-        
-        comparator : function(model) {
-            return Date.parse(model.created_at);
         }
     });
     
-    models.Timeline = Timeline;
+    models.TimelineBase = TimelineBase;
     
 }, "@VERSION@", {
     requires : [
         // YUI
         "base-build",
-        "lazy-model-list",
+        "model",
         
         // Models
-        //"model-tweet"
+        "model-list-tweets"
     ]
 });
