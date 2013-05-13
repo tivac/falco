315dd0f059e2685d111f9c5fa672c6b4fbdccff9
Pat Cavit <pcavit@gmail.com>
2013-05-13 01:06:44 -0700

    Home timeline getting tweets again
    Also listening to stream updates w/o doing much

--- src/js/model-timeline-home.js
+++ src/js/model-timeline-home.js
@@ -1,10 +1,23 @@
 YUI.add("model-timeline-home", function(Y) {
     "use strict";
     
-    var models = Y.namespace("Tristis.Models"),
+    var models  = Y.namespace("Tristis.Models"),
+        streams = Y.namespace("Tristis.Streams"),
         Home;
         
-    Home = Y.Base.create("home", models.TimelineBase, [], {}, {
+    Home = Y.Base.create("home", models.TimelineBase, [], {
+        initializer : function() {
+            streams.user.on("tweet", this._streamTweet, this);
+            
+            this.get("tweets").setAttrs({
+                api : "statuses/home_timeline"
+            });
+        },
+        
+        _streamTweet : function(e) {
+            console.log("User stream tweet in home timeline", e.type, e);
+        }
+    }, {
         ATTRS : {
             id : {
                 value : "home"
@@ -20,6 +33,9 @@ YUI.add("model-timeline-home", function(Y) {
         "base-build",
         
         // Models
-        "model-timeline-base"
+        "model-timeline-base",
+        
+        // Streams
+        "stream-user"
     ]
 });
