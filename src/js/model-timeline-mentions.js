169aa2c14d6f6d0132376c75372758c3ad770266
Pat Cavit <pcavit@gmail.com>
2013-05-15 00:36:53 -0700

    Mentions timeline model

--- src/js/model-timeline-mentions.js
+++ src/js/model-timeline-mentions.js
@@ -1,10 +1,41 @@
 YUI.add("model-timeline-mentions", function(Y) {
     "use strict";
     
-    var models = Y.namespace("Tristis.Models"),
+    var models  = Y.namespace("Tristis.Models"),
+        streams = Y.namespace("Tristis.Streams"),
         Mentions;
         
-    Mentions = Y.Base.create("mentions", models.TimelineBase, [], {}, {
+    Mentions = Y.Base.create("mentions", models.TimelineBase, [], {
+        initializer : function() {
+            this._handles.push(
+                streams.user.on("tweet", this._streamTweet, this)
+            );
+            
+            this.get("tweets").setAttrs({
+                api : "statuses/mentions_timeline"
+            });
+        },
+        
+        _streamTweet : function(e) {
+            var mentions = e.tweet.entities.user_mentions,
+                id       = models.user.get("id_str"),
+                mentioned;
+            
+            if(!mentions || !mentions.length) {
+                return;
+            }
+            
+            mentioned = mentions.some(function(mention) {
+                return mention.id_str === id;
+            });
+            
+            if(!mentioned) {
+                return;
+            }
+            
+            this.get("tweets").add(e.tweet);
+        }
+    }, {
         ATTRS : {
             id : {
                 value : "mentions"
