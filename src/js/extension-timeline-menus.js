95c0d6c97517a2700e62ddc408b5f2f96f82271f
Pat Cavit <pcavit@gmail.com>
2013-05-21 16:04:04 -0700

    Working context menus

--- src/js/extension-timeline-menus.js
+++ src/js/extension-timeline-menus.js
@@ -20,62 +20,102 @@ YUI.add("extension-timeline-menus", function(Y) {
     
     Menus = function() {};
     Menus.prototype = {
+        menus : {
+            username : [
+                { label : "User Details",  click : "_followLink" },
+                { label : "User Mentions", click : "_userSearch" },
+                { type : "separator" },
+                { label : "Copy Username", click : "_copyUser" },
+                { label : "Copy User URL", click : "_copyLink" }
+                
+            ],
+            
+            hashtag : [
+                { label : "Hashtag Tweets", click : "_followLink" },
+                { type : "separator" },
+                { label : "Copy Hashtag",   click : "_copyText" }
+            ],
+            
+            link : [
+                { label : "Open URL", click : "_followLink" },
+                { type : "separator" },
+                { label : "Copy URL", click : "_copyLink" }
+            ]
+        },
+        
         initializer : function() {
             this.events = Y.merge(this.events || {}, _events);
+            
             this._menus = {};
         },
         
         // Menu setup
-        _setupUsernameMenu : function() {
-            var menu = new gui.Menu();
+        _setupMenu : function(type) {
+            var self = this,
+                menu;
             
-            menu.append(new gui.MenuItem({
-                label : "User Details"
-            }));
+            if(this._menus[type]) {
+                return this._menus[type];
+            }
             
-            menu.append(new gui.MenuItem({
-                label : "Copy Username"
-            }));
+            menu = new gui.Menu();
             
-            menu.append(new gui.MenuItem({
-                label : "Search for User"
-            }));
+            this._menus[type] = menu;
             
-            this._menus.username = menu;
+            this.menus[type].forEach(function(item) {
+                if(item.click) {
+                    item.click = self[item.click].bind(self);
+                }
+                
+                menu.append(new gui.MenuItem(item));
+            });
             
             return menu;
         },
         
-        // Menu Events
+        _showMenu : function(type, e) {
+            e.preventDefault();
+            
+            // Store current element because node-webkit menu items don't know
+            // anything about their state
+            this._element = e.currentTarget;
+            
+            // Go get/create menu & show it at mouse position
+            this._setupMenu(type).popup(e.clientX, e.clientY);
+        },
         
+        // Menu Events
+        _followLink : function() {
+            gui.Shell.openExternal(this._element.get("href"));
+        },
+        
+        _copyLink : function() {
+            clipboard.set(this._element.get("title"));
+        },
+        
+        _copyText : function() {
+            clipboard.set(this._element.get("text"));
+        },
+        
+        _copyUser : function() {
+            clipboard.set("@" + this._element.get("text"));
+        },
+        
+        _userSearch : function() {
+            gui.Shell.openExternal("https://twitter.com/search?q=" + encodeURIComponent(this._element.get("text")));
+        },
         
         // DOM Events
         _usernameMenu : function(e) {
-            var menu;
-            
-            e.preventDefault();
-            
-            console.log("user name", e);
-            
-            menu = this._menus.username;
-            
-            if(!menu) {
-                menu = this._setupUsernameMenu();
-            }
-            
-            menu.popup(e.clientX, e.clientY);
+            this._showMenu("username", e);
         },
         
         _hashtagMenu : function(e) {
-            e.preventDefault();
-            
-            console.log("hash tag", e);
+            this._showMenu("hashtag", e);
         },
         
         _linkMenu : function(e) {
-            e.preventDefault();
-            
-            console.log("link", e);
+            this._showMenu("link", e);
         },
     };
     
