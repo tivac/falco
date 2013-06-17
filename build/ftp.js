af3520fa9f5aa747ea46e48ddcb68399c60b66a7
Pat Cavit <pcavit@gmail.com>
2013-06-17 01:39:37 -0700

    Show a progress bar for FTP uploads

--- build/ftp.js
+++ build/ftp.js
@@ -3,20 +3,31 @@
 
 module.exports = function(grunt) {
     
-    var fs   = require("fs"),
-        Ftp  = require("jsftp"),
-        conf = require("config").Deploy;
+    var fs       = require("fs"),
+        Ftp      = require("jsftp"),
+        conf     = require("config").Deploy,
+        Progress = require("progress");
     
     grunt.registerTask("ftp", "Copy the current version zip to a FTP server", function() {
         var done = this.async(),
             ftp  = new Ftp(conf),
             file = "tristis-v" + grunt.config.get("pkg").version + ".zip",
-            release;
+            path = "./bin/" + file,
+            bar, release;
         
-        release = fs.createReadStream("./bin/" + file);
+        release = fs.createReadStream(path);
         release.pause();
         
+        grunt.log.writeln("Uploading ./bin/" + file + " to " + conf.dir + file);
+        bar = new Progress("[:bar] :percent", {
+            incomplete : " ",
+            width      : "50",
+            total      : fs.statSync(path).size
+        });
+        
         ftp.getPutSocket(conf.dir + file, function(err, socket) {
+            var written = 0;
+            
             if (err) {
                 grunt.log.error(err);
                 
@@ -31,9 +42,15 @@ module.exports = function(grunt) {
             
             release.pipe(socket); // Transfer from source to the remote file
             
-            grunt.log.writeln("Streaming ./bin/" + file + " to " + conf.dir + file);
+            
             
             release.resume();
+            
+            socket.on("drain", function() {
+                bar.tick(socket.bytesWritten - written);
+                
+                written = socket.bytesWritten;
+            });
         });
     });
 };
