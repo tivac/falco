bd28a010ceb8abeb1cf86917ffe159c1006e8a65
Pat Cavit <pcavit@gmail.com>
2013-12-01 15:33:25 -0800

    Saved searches have IDs, so stop mucking around

--- src/js/models/model-timeline-search.js
+++ src/js/models/model-timeline-search.js
@@ -1,4 +1,3 @@
-/*jshint node:true, browser:true, yui:true */
 YUI.add("model-timeline-search", function(Y) {
     "use strict";
     
@@ -10,27 +9,17 @@ YUI.add("model-timeline-search", function(Y) {
         initializer : function(config) {
             config || (config = {});
             
-            // Keep ID updated to match encoded query param
-            this._handles = [
-                this.on("queryChange", this._queryChange, this)
-            ];
-            
-            // Force-update ID to match encoded query param
-            this._queryChange({ newVal : config.query || this.get("query") });
-            
             this.get("tweets").setAttrs({
-                api    : "search/tweets",
+                selector : "statuses",
+                api      : "search/tweets",
+                
                 config : {
-                    q     : this.get("id"),
+                    q     : this.get("query"),
                     count : 100
                 }
             });
             
             this.set("url", "/searches/" + this.get("id"));
-        },
-        
-        _queryChange : function(e) {
-            this.set("id", encodeURIComponent(e.newVal));
         }
     }, {
         ATTRS : {
