91bb5bfd33471e35b7b6412112ada4e330a5dba3
Pat Cavit <pcavit@gmail.com>
2013-12-03 13:16:23 -0800

    Poll searches for updates every 30s
    Should be configurable/respect rate limits some day.

--- src/js/models/model-timeline-search.js
+++ src/js/models/model-timeline-search.js
@@ -9,6 +9,10 @@ YUI.add("model-timeline-search", function(Y) {
         initializer : function(config) {
             config || (config = {});
             
+            this._handles.push(
+                this.get("tweets").after("load", this._poll, this)
+            );
+            
             this.get("tweets").setAttrs({
                 selector : "statuses",
                 api      : "search/tweets",
@@ -20,6 +24,12 @@ YUI.add("model-timeline-search", function(Y) {
             });
             
             this.set("url", "/searches/" + this.get("id"));
+        },
+        
+        _poll : function() {
+            Y.later(30000, this, function() {
+                this.get("tweets").more(this._poll.bind(this));
+            });
         }
     }, {
         ATTRS : {
