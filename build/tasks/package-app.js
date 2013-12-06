b4d88530ff40724977beb3defef116874a8b41a1
Pat Cavit <pcavit@gmail.com>
2013-12-05 16:23:43 -0800

    Change how platforms are handled

--- build/tasks/package-app.js
+++ build/tasks/package-app.js
@@ -1,10 +1,50 @@
 /*jshint node:true */
 "use strict";
 
-module.exports = function(config, done) {
-    if(!config.dullard.tasks["package-app-" + config.platform]) {
-        return done("No package-app-" + config.platform + " task has been defined, please write one");
-    }
+var fs    = require("fs"),
+    async = require("async"),
     
-    config.dullard.run("package-app-" + config.platform, done);
+    platforms;
+
+module.exports = function(config, done) {
+    platforms[config.platform](config, done);
+};
+
+platforms = {
+    win : function(config, done) {
+        async.waterfall([
+            function readNodeWebkit(callback) {
+                fs.readFile(
+                    "./temp/nw/nw.exe",
+                    function(err, data) {
+                        if(err) {
+                            return callback(err);
+                        }
+                    
+                        callback(null, data);
+                    }
+                );
+            },
+            
+            function readTristis(nw, callback) {
+                fs.readFile("./temp/" + config.app + ".nw", function(err, data) {
+                    if(err) {
+                        return callback(err);
+                    }
+                    
+                    callback(null, nw, data);
+                });
+            },
+            
+            function combine(nw, falco, callback) {
+                callback(null, Buffer.concat([ nw, falco ]));
+            },
+            
+            function write(falco, callback) {
+                fs.writeFile("./dist/" + config.app + ".exe", falco, function(err) {
+                    callback(err);
+                });
+            }
+        ], done);
+    }
 };
