f1947b958d710aac72aae9cb4b8ad7fe007b3652
Pat Cavit <pcavit@gmail.com>
2015-01-21 23:06:36 -0800

    Tweaking, still not sure about immutable.

--- src/app.js
+++ src/app.js
@@ -12,12 +12,35 @@ function App() {
 
 util.inherits(App, EventEmitter);
 
+App.prototype.getLists = function() {
+    var self = this;
+    
+    twitter.get("lists/list", function(error, lists) {
+        if(error) {
+            console.error(error);
+            
+            return;
+        }
+        
+        lists.forEach(function(list) {
+            self.addList(list);
+        });
+    });
+};
+
 App.prototype.addList = function(list) {
     this._state = this._state.update("lists", function(lists) {
         return lists.push(list);
     });
     
     this.emit("change", this._state);
+};
+
+App.prototype.loadTweets = function() {
+    var selected = this._state.get("selected"),
+        list     = this._state.get("lists").get(selected);
+        
+    console.log(selected, list);
 };
 
 module.exports = App;
