034608abb21346ea38921d5db5de786174f6150c
Pat Cavit <pcavit@gmail.com>
2015-02-17 23:40:25 -0800

    Support retweets better
    Also visually represent newlines in tweets

--- src/tweet.js
+++ src/tweet.js
@@ -3,16 +3,24 @@
 var twitter = require("twitter-text"),
     twemoji = require("twemoji");
 
+function getSource(tweet) {
+    return tweet.retweeted_status? tweet.retweeted_status : tweet;
+}
+
 exports.parse = function(tweet) {
+    var source = getSource(tweet),
+        text   = twitter.autoLinkWithJSON(
+            source.text,
+            source.entities
+        );
+    
+    // respect newlines
+    text = text.replace(/\n|\r\n|\r/g, "<br />");
+    
     return twemoji.parse(
-        twitter.autoLinkWithJSON(
-            tweet.text,
-            tweet.entities
-        ),
-        function(icon, options, variant) {
-            console.log(options);
-            
+        text,
+        function(icon) {
             return "../node_modules/twemoji/svg/" + icon + ".svg";
         }
     );
-}
+};
