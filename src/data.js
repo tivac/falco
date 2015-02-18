a22ed8b48d5c5ca3a791b90cf94c53e4aa49144a
Pat Cavit <pcavit@gmail.com>
2015-02-17 23:41:28 -0800

    Groundwork for streaming user tweets

--- src/data.js
+++ src/data.js
@@ -9,8 +9,10 @@ var EventEmitter = require("events").EventEmitter,
 
 function Data() {
     this._state = immutable({
-        selected : "timeline",
-        lists : {
+        active : "timeline",
+        users  : {},
+        order  : [ "timeline", "notifications" ],
+        lists  : {
             timeline : {
                 name    : "Timeline",
                 uri     : "/home",
@@ -22,16 +24,16 @@ function Data() {
                 uri     : "/notifications",
                 tweets  : []
             }
-        },
-        order : [ "timeline", "notifications" ]
+        }
     });
     
     this.loadLists();
-    this.loadTweets();
+    this.loadTweets(this._state.active);
 }
 
 util.inherits(Data, EventEmitter);
 
+// Util
 Data.prototype._changed = function() {
     this.emit("change", this._state);
 };
@@ -40,12 +42,13 @@ Data.prototype.get = function(key) {
     return this._state[key];
 };
 
+// Loading
 Data.prototype.loadLists = function() {
     var self = this;
     
-    twitter.get("lists/list", function(error, lists) {
-        if(error) {
-            return console.error(error);
+    twitter.get("lists/list", function(err, lists) {
+        if(err) {
+            return console.log(err);
         }
         
         lists.forEach(function(list) {
@@ -54,30 +57,67 @@ Data.prototype.loadLists = function() {
     });
 };
 
-Data.prototype.loadTweets = function() {
-    var self     = this,
-        selected = this._state.selected,
+Data.prototype.loadTweets = function(list) {
+    var self = this,
+        options = {
+            count       : 200,
+            include_rts : true
+        },
         url;
         
-    if(selected === "timeline") {
+    if(list === "timeline") {
         url = "statuses/home_timeline";
+    } else if(list === "notifications") {
+        url = "statuses/mentions_timeline";
+    } else {
+        url = "lists/statuses";
+        options.list_id = list;
     }
     
-    twitter.get(url, function(error, tweets) {
-        if(error) {
-            console.log(error);
-            
-            return;
+    twitter.get(url, options, function(err, tweets) {
+        if(err) {
+            return console.log(err);
         }
         
-        self.addTweets(selected, tweets);
+        self.addTweets(list, tweets);
     });
 };
 
-Data.prototype.addList = function(list) {
-    var lists = {};
+Data.prototype.loadUsers = function(list) {
+    var self = this;
     
-    lists[list.id_str] = {
+    // No-ops in the special lists case, this is handled by the user stream
+    if(list === "timeline" || list === "notifications") {
+        return;
+    }
+    
+    twitter.get(
+        "lists/members", {
+            list_id     : list,
+            count       : 5000,
+            skip_status : true
+        },
+        function(err, resp) {
+            if(err) {
+                return console.log(err);
+            }
+            
+            self.addUsers(
+                resp.users.map(function(user) {
+                    return user.id_str;
+                }),
+                list.id_str
+            );
+        }
+    );
+};
+
+// Mutations
+Data.prototype.addList = function(list) {
+    var id    = list.id_str,
+        lists = {};
+    
+    lists[id] = {
         name   : list.name,
         uri    : list.uri,
         tweets : []
@@ -85,7 +125,24 @@ Data.prototype.addList = function(list) {
     
     this._state = this._state.merge({
         lists : this._state.lists.merge(lists),
-        order : this._state.order.concat([ list.id_str ])
+        order : this._state.order.concat([ id ])
+    });
+    
+    // Go get users for this list to track
+    this.loadUsers(id);
+    
+    this._changed();
+};
+
+Data.prototype.addUsers = function(users, list) {
+    var map = {};
+    
+    users.forEach(function(id) {
+        map[id] = list;
+    });
+    
+    this._state = this._state.merge({
+        users : this._state.users.merge(map)
     });
     
     this._changed();
@@ -108,11 +165,10 @@ Data.prototype.addTweets = function(key, tweets) {
 
 Data.prototype.selectList = function(list) {
     this._state = this._state.merge({
-        selected : list
+        active : list
     });
     
     this._changed();
 };
-
 
 module.exports = new Data();
