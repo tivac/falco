cab3a71ea2396c4c3de715abf34c074955888fbf
Pat Cavit <pcavit@gmail.com>
2015-02-24 23:37:14 -0800

    Remove unused code

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/*global Mousetrap:true */
 "use strict";
 
 require("autostrip-json-comments");
@@ -6,29 +5,13 @@ require("autostrip-json-comments");
 var state = require("./state"),
     m     = require("mithril"),
     
-    gui   = require("nw.gui"),
-    argv  = require("minimist")(gui.App.argv);
+    gui   = require("nw.gui");
 
-// Debug helpers
-if(argv.debug) {
-    window.state = state;
+window.state = state;
 
-    Mousetrap.bind("ctrl+shift+r", function() {
-        gui.Window.get().reloadDev();
-    });
-    
-    Mousetrap.bind("ctrl+shift+k", function() {
-        var win = gui.Window.get();
-        
-        win[win.isDevToolsOpen() ? "closeDevTools" : "showDevTools"]();
-    });
-    
-    gui.Window.get().showDevTools();
-}
+gui.Window.get().showDevTools();
 
-state.on("change", function(current) {
-    console.log("State Change", current.asMutable({ deep : true }));
-    
+state.on("change", function() {
     m.redraw();
 });
 
