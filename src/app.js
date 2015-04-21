b20d76ebe04162b17137385017b59c7aafd09b74
Pat Cavit <pcavit@gmail.com>
2015-04-20 23:58:23 -0700

    Minor clean up

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 "use strict";
 
-var //state = require("./lib/state"),
+var state = require("./lib/state"),
     m     = require("mithril"),
     
     gui   = require("nw.gui");
 
-//window.state = state;
+window.state = state;
 
 gui.Window.get().showDevTools();
 
-/*state.on("change", function() {
+state.on("change", function() {
     m.redraw();
-});*/
+});
 
 m.route(
     document.querySelector("#layout"),
